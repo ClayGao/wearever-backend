df21816bc6db81057e78fb74b6208a316f235189
Clay Kao <clay.kao@positivegrid.com>
2024-02-18 02:14:25 +0800

    style: comment out unused imports and variables

--- sheet/getProductsToSheet.ts
+++ sheet/getProductsToSheet.ts
@@ -1,10 +1,12 @@
-import ProductList from "@/product-2024-02-11-to-2024-02-03-women.json";
+// import ProductList from "@/product-2024-02-11-to-2024-02-03-women.json";
 // import * as ProductList from "../laroom.json";
 // import * as ProductList from "../veryu.json";
 
 import { postToStagingSheet } from "@/api/sheet";
 import { chromium } from "@playwright/test";
 import { PLAYWRIGHT_CONFIG, SITE_CONFIG } from "@/wearever.config";
+
+const ProductList: any[] = []
 
 const init = async () => {
   const browser = await chromium.launch({
